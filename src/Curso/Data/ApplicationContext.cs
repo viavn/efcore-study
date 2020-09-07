af80d2bf44dded0daa7b9cb7e502c2ac2fcf14f6
Vinicius Avansini <vi.avn@hotmail.com>
2020-09-07 12:06:01 -0300

    Added EF Logging and CRUD operations

--- src/Curso/Data/ApplicationContext.cs
+++ src/Curso/Data/ApplicationContext.cs
@@ -1,16 +1,23 @@
 using CursoEFCore.Domain;
 using CursoEFCore.Util;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CursoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(Configurations.ConnectionString);
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(Configurations.ConnectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- src/Curso/Program.cs
+++ src/Curso/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CursoEFCore.Data;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore
@@ -9,16 +12,175 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            using var db = new ApplicationContext();
+            // using var db = new ApplicationContext();
             // Dangerous executing on prod
             // db.Database.Migrate();
-            var hasPendingMigration = db.Database.GetPendingMigrations().Any();
-            if (hasPendingMigration)
+
+            // var hasPendingMigration = db.Database.GetPendingMigrations().Any();
+            // if (hasPendingMigration)
+            // {
+            //     // logic..
+            // }
+
+            // InsertProduct();
+            // ProductBulkInsert();
+            // GetClientes();
+            // InsertPedido();
+            // GetPedidoEagerLoading();
+            // UpdateClient();
+            DeleteClient();
+        }
+
+        private static void DeleteClient()
+        {
+            using var db = new Data.ApplicationContext();
+            // var cliente = db.Clientes.Find(3);
+
+            var cliente = new Cliente { Id = 4 };
+
+            // db.Clientes.Remove(cliente);
+            // db.Remove(cliente);
+            db.Entry(cliente).State = EntityState.Deleted;
+
+            db.SaveChanges();
+        }
+
+        private static void UpdateClient()
+        {
+            using var db = new Data.ApplicationContext();
+            var cliente = db.Clientes.Find(2);
+            // cliente.Nome = "Cliente Alterado Passo 4";
+            cliente.Cidade = "SBO";
+
+            // var cliente = new Cliente { Id = 2 };
+            // var clienteDesconectado = new
+            // {
+            //     Nome = "Cliente Desconectado Passo 3",
+            //     Telefone = "19999999966"
+            // };
+
+            // db.Attach(cliente);
+            // db.Entry(cliente).CurrentValues.SetValues(clienteDesconectado);
+
+            // Atualiza todas as propriedades
+            // db.Clientes.Update(cliente);
+            // db.Entry(cliente).State = EntityState.Modified;
+
+            // Se apenas chamar SaveChanges(), irá fazer update só nas propriedades que mudaram
+            db.SaveChanges();
+        }
+
+        // Carregamento Adiantado
+        private static void GetPedidoEagerLoading()
+        {
+            using var db = new Data.ApplicationContext();
+            var pedidos = db.Pedidos
+                .Include(pedido => pedido.Itens)
+                .ThenInclude(item => item.Produto)
+                .ToList();
+
+            Console.WriteLine(pedidos.Count);
+        }
+
+        private static void InsertPedido()
+        {
+            using var db = new Data.ApplicationContext();
+
+            var cliente = db.Clientes.FirstOrDefault();
+            var produto = db.Produtos.FirstOrDefault();
+
+            var pedido = new Pedido
             {
-                // logic..
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                FinalizadoEm = DateTime.Now,
+                Observacao = "Pedido teste",
+                Status = StatusPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+                Itens = new List<PedidoItem>
+                {
+                    new PedidoItem
+                    {
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10
+                    }
+                }
+            };
+
+            db.Pedidos.Add(pedido);
+            db.SaveChanges();
+        }
+
+        private static void GetClientes()
+        {
+            using var db = new Data.ApplicationContext();
+            // var queryByLINQ = (from cliente in db.Clientes where cliente.Id > 0 select cliente)
+            //     .ToList();
+            var queryByExtesion = db.Clientes
+                // .AsNoTracking()
+                .Where(cliente => cliente.Id > 0)
+                .OrderBy(cliente => cliente.Id)
+                .ToList();
+
+            foreach (var cliente in queryByExtesion)
+            {
+                Console.WriteLine($"Consultando cliente: {cliente.Id}");
+                // Apenas Find que é o responsável por consultar se o objeto está em memória ou não
+                // db.Clientes.Find(cliente.Id);
+                // Vai consultar direto na base
+                db.Clientes.FirstOrDefault(cli => cli.Id == cliente.Id);
             }
 
-            Console.WriteLine("Hello World!");
+        }
+
+        private static void ProductBulkInsert()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "01234567891234",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MarcadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Vinicius",
+                CEP = "99999999",
+                Cidade = "Americana",
+                Estado = "SP",
+                Telefone = "19999999999"
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.AddRange(produto, cliente);
+
+            var registers = db.SaveChanges();
+            Console.WriteLine($"Total: {registers}");
+        }
+
+        private static void InsertProduct()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "01234567891234",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MarcadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.Produtos.Add(produto);
+            // db.Set<Produto>().Add(produto);
+            // db.Entry(produto).State = EntityState.Added;
+            // db.Add(produto);
+
+            var registers = db.SaveChanges();
+            Console.WriteLine($"Registros alterados: {registers}");
         }
     }
 }
